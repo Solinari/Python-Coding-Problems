a8a0398c9f7baee8531f53a9b571ea029545c57e
Justin Tyme Dejesus <justin.tyme.dejesus@gmail.com>
2014-07-13 22:26:41 -0500

    Added comments!

--- the zeros at the end problem.py
+++ the zeros at the end problem.py
@@ -1,12 +1,17 @@
 
+# initial list
 a = [0,1,2,6,8,0,6,7,9,0,4]
 
+# countdown variable
 x = a.count(0)
 
+# while loop will continually remove and append
+# 0's until our intital count value of the number
+# of zeroes reaches 0
 while x > 0:
     a.remove(0)
     a.append(0)
     x -= 1
     print(a)
 
-print(a)
+
